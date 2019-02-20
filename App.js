e550c4619627996892c641995b766f111c3a8430
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-20 11:31:59 -0800

    installed firebase for authentication and installed redux to craetstore for the app

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
+import reducers from './src/reducers';
 
-export default class App extends React.Component {
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyA8QJ1Lzfa_4AnJ0S_t8am5U_uoHAdaN18',
+      authDomain: 'manager-a2121.firebaseapp.com',
+      databaseURL: 'https://manager-a2121.firebaseio.com',
+      projectId: 'manager-a2121',
+      storageBucket: 'manager-a2121.appspot.com',
+      messagingSenderId: '889970175203'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
