42927cf2f511672683cc9954842a126691c5a4c3
AJ Arriola <angelojames.arriola@gmail.com>
2019-02-25 13:34:45 -0800

    loginUser function uses firebase to pass email and password to verify and pass login user success as a type with the payload to the reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
+import firebase from 'firebase';
+
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -20,6 +23,11 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(console.log(user))
+      .then(user => {
+        dispatch({
+          type: LOGIN_USER_SUCCESS,
+          payload: user
+        })
+      })
   }
 }
