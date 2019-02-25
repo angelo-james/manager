ab59a377290f5ea5f4929646cdbd363177d87d1f
AJ Arriola <angelojames.arriola@gmail.com>
2019-02-25 10:16:52 -0800

    created types file in actions for and imported the email changed action into the index and the authReducer files

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,15 @@
+import { 
+  EMAIL_CHANGED
+} from '../actions/types';
+
 const initialState = {
   email: ''
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      
     default:
       return state;
   }
