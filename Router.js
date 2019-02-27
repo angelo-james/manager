e62d6dce87a65bd7f7f78b2d54f4dabe83dcb0da
AJ Arriola <angelojames.arriola@gmail.com>
2019-02-27 09:15:02 -0800

    seperated auth and main scenes in router file

--- Router.js
+++ Router.js
@@ -6,19 +6,25 @@ import EmployeeList from './src/components/EmployeeList';
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene
-          key="login"
-          component={LoginForm}
-          title="Please Login"
-          initial
-        />
+      <Scene key="root" hideNavBar>
 
-        <Scene 
-          key="employeeList"
-          component={EmployeeList}
-          title="Employees"
-        />
+        <Scene key="auth">
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+            initial
+          />
+        </Scene>
+
+        <Scene key="main">
+          <Scene 
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+          />
+        </Scene>
+
       </Scene>
     </Router>
   )
