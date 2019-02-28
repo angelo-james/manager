46996d10c414b0eb1fff3f1a2d88882ab580e10c
AJ Arriola <angelojames.arriola@gmail.com>
2019-02-28 11:30:32 -0800

    set up employee update case in reducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,11 +3,18 @@ import {
 } from '../actions/types';
 
 const initialState = {
-
+  name: '',
+  phone: '',
+  shift: ''
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      return {
+        ...state,
+        [action.payload.prop]: action.payload.value
+      }
     default:
       return state;
   }
