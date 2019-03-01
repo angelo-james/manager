f753bcb19f3d360edc88939c3ad9f1c0b627ca37
AJ Arriola <angelojames.arriola@gmail.com>
2019-02-28 16:46:06 -0800

    imported firebase into my employee actions file. sending user input to my firebase db

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -13,5 +15,8 @@ export const employeeUpdate = ({ prop, value }) => {
 }
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth();
+
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift })
 }
