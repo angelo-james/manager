70733e20e8139a370fafdddcc87134fe4280ecc0
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-02 12:02:08 -0800

    created a reusable form component and refactored employee create component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate, employeeCreate } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
   onButtonPress() {
@@ -14,41 +14,9 @@ class EmployeeCreate extends Component {
   render() {
     return (
       <Card>
-        <CardSection>
-          <Input
-            label='Name'
-            placeholder='Jane'
-            value={this.props.name}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input 
-            label='Phone'
-            placeholder='555-555-5555'
-            value={this.props.phone}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-          />
-        </CardSection>
-
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerTextStyle}>Shift</Text>
-          <Picker
-            style={{ flex: 1 }}
-            selcetedValue={this.props.shift}
-            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-          >
-            <Picker.Item key='Monday' label='Monday' value='Monday' />
-            <Picker.Item key='Tuesday' label='Tuesday' value='Tuesday' />
-            <Picker.Item key='Wednesday' label='Wednesday' value='Wednesday' />
-            <Picker.Item key='Thursday' label='Thursday' value='Thursday' />
-            <Picker.Item key='Friday' label='Friday' value='Friday' />
-            <Picker.Item key='Saturday' label='Saturday' value='Saturday' />
-            <Picker.Item key='Sunday' label='Sunday' value='Sunday' />
-          </Picker>
-        </CardSection>
-
+        <EmployeeForm 
+          {...this.props}
+        />
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Create
@@ -56,13 +24,6 @@ class EmployeeCreate extends Component {
         </CardSection>
       </Card>
     )
-  }
-}
-
-const styles = {
-  pickerTextStyle: {
-    fontSize: 18,
-    paddingLeft: 20
   }
 }
 
