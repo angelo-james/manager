942566669d1aec8613e9a47371e4c16d02d2f5f5
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-02 19:02:13 -0800

    created a button that toggles the modal to be visible

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,9 +4,13 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = {
+    showModal: false
+  }
+
   componentWillMount() {
     _.forEach(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value })
@@ -44,6 +48,20 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => this.setState({ showModal: !this.state.showModal })}
+          >
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to fire this employee?
+        </Confirm>
       </Card>
     )
   }
