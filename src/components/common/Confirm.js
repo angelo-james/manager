059c9bfb77c70aad3c0c911b16c09f82b6674223
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-02 18:11:49 -0800

    created new common reusable component to be a modal

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = () => {
+  
+}
+
+export { Confirm };
